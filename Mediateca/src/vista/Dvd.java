4f5944bd563b8aad98bf34a59c682c40befdf69e
hector01mt <marquez_mt02@outlook.com>
2024-05-06 22:02:57 -0600

    mediateca

---
+++ Mediateca/src/vista/Dvd.java
@@ -0,0 +1,10 @@
+package vista;
+
+import javax.swing.*;
+
+public class Dvd {
+    private JButton librosButton1;
+    private JButton revistasButton;
+    private JButton cdsButton;
+    private JButton dvdButton;
+}
